b93dee38533bb45fe1ed123eebaea9100b38de57
Winger1994 <zhaochaowins@icloud.com>
2016-10-02 17:12:37 +0800

    Initial Commit

---
+++ MyShell/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MyShell
+//
+//  Created by 赵超 on 02/10/2016.
+//  Copyright © 2016 赵超. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
